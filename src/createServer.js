92eeca5efb3f104e3c1302705e0202d135a20bb4
IShamkii <fanmetalheavy@gmail.com>
2025-02-28 21:41:35 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -7,18 +7,26 @@ const mime = require('mime-types');
 function createServer() {
   return http.createServer((req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
-    const contentType = mime.lookup(url.pathname) || 'text/plain';
+    const initialUrl = url.pathname;
+    const contentType = mime.lookup(initialUrl) || 'text/plain';
 
     res.setHeader('Content-Type', contentType);
 
-    if (req.url === '/file') {
+    if (initialUrl === '/file') {
       res.statusCode = 200;
       res.end('Please provide a file path. Example: /file/example.txt');
 
       return;
     }
 
-    const pathToFile = url.pathname.slice(1).replace('file', '');
+    if (!initialUrl.startsWith('/file') || initialUrl.includes('..')) {
+      res.statusCode = 400;
+      res.end('Invalid file path');
+
+      return;
+    }
+
+    const pathToFile = `./public/${initialUrl.slice(1).replace('file/', '')}`;
 
     if (pathToFile.includes('//')) {
       res.statusCode = 404;
@@ -27,20 +35,24 @@ function createServer() {
       return;
     }
 
-    try {
-      const readFile = fs.readFileSync(`./public${pathToFile}`, 'utf8');
+    fs.readFile(pathToFile, 'utf8', (err, data) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('No such file or directory');
 
-      res.statusCode = 200;
-      res.end(readFile);
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        res.statusCode = 404;
-        res.end('No such file or directory');
+          return;
+        }
+
+        res.statusCode = 400;
+        res.end();
+
+        return;
       }
 
-      res.statusCode = 400;
-      res.end();
-    }
+      res.statusCode = 200;
+      res.end(data);
+    });
   });
 }
 
